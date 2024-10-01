652bb5ebb7dd8912ff446ae44dbf4ba405026783
EC2 Default User <ec2-user@ip-172-31-62-217.ec2.internal>
2024-10-01 14:58:10 +0000

    first draft of dynamoDbmanager implemented

--- week3/dynamo_db.py
+++ week3/dynamo_db.py
@@ -28,3 +28,60 @@ class DynamoDb:
         except ClientError as e:
             logging.error(f"Error connecting to AWS: {e}")
             raise e
+    
+    def create_table(self, table_name, key_schema, attribute_definitions, provisioned_throughput, region):
+        
+        try:
+            dynamodb_resource = boto3.resource("dynamodb", region_name=region)
+            logging.info(f"\ncreating the table {table_name} ...")
+            self.table = dynamodb_resource.create_table(TableName=table_name, KeySchema=key_schema, AttributeDefinitions=attribute_definitions,
+                ProvisionedThroughput=provisioned_throughput)
+
+            # Wait until the table exists.
+            self.table.meta.client.get_waiter('table_exists').wait(TableName=table_name)
+            
+        except ClientError as e:
+            logging.error(e)
+            return False
+        return True
+
+ 
+        
+
+    def store_an_item(self, region, table_name, item):
+        try:
+            logging.info("\nstoring the item {item} in the table {table_name} ...")
+            dynamodb_resource = boto3.resource("dynamodb", region_name=region)
+            table = dynamodb_resource.Table(table_name)
+            table.put_item(Item=item)
+        
+        except ClientError as e:
+            logging.error(e)
+            return False
+        return True
+        
+        
+     
+    def get_an_item(self, region, table_name, key):
+        try:
+            logging.info("\nretrieving the item with the key {key} from the table {table_name} ...")
+            dynamodb_resource = boto3.resource("dynamodb", region_name=region)
+            table = dynamodb_resource.Table(table_name)
+            response = table.get_item(Key=key)
+            item = response['Item']
+            print(item)
+        
+        except ClientError as e:
+            logging.error(e)
+            return False
+        return True
+        
+    def update_an_item(self,table_name,key,update_attributes,new_attribute_values,region):
+        try:
+            dynamodb_resource = boto3.resource("dynamodb", region_name=region)
+            table = dynamodb_resource.Table(table_name)
+            table.update_item(TableName=table_name,Key=key,UpdateExpression=update_attributes,ExpressionAttributeValues=new_attribute_values)
+            logging.info(f"updating item:{update_attributes} with values:{new_attribute_values}")
+        
+        except Exception as e:
+            logging.error(f"Error updating table items: {e}")
--- week3/main.py
+++ week3/main.py
@@ -2,7 +2,87 @@ from dynamo_db import DynamoDb
 
 
 def main():
+ 
+    
+    # TASK: Create a DynamoDB table
+    region = 'us-east-1'
+    table_name="music"
+    
     db_client = DynamoDb()
-
+    
+    """
+    key_schema=[
+        {
+            "AttributeName": "artist",
+            "KeyType": "HASH"
+        },
+        {
+            'AttributeName': 'song',
+            'KeyType': 'RANGE'
+        }
+    ]
+    
+    attribute_definitions=[
+        {
+            "AttributeName": "artist",
+            "AttributeType": "S"
+        },
+        {
+            "AttributeName": "song",
+            "AttributeType": "S"
+        }
+        
+    ]
+    
+    provisioned_throughput={
+        "ReadCapacityUnits": 1,
+        "WriteCapacityUnits": 1
+    }
+    
+    db_client.create_table(table_name, key_schema, attribute_definitions, provisioned_throughput, region)
+   
+    
+    # TASK: Store data (items) into a DynamoDB table
+    item = {
+        "artist": "Pink Floyd",
+        "song": "Us and Them",
+        "album": "The Dark Side of the Moon",
+        "year": 1973
+    }
+    
+    #add item to table
+    db_client.store_an_item(region, table_name, item)
+    
+    item = {
+        "artist": "Michael Jackson",
+        "song": "Billie Jean",
+        "album": "Thriller",
+        "length_seconds": 294
+    }
+    
+    #add item to table
+    db_client.store_an_item(region, table_name, item)
+    
+    # TASK: Retrieve the attributes of the item with the given primary key
+    
+    key_info={
+        "artist": "Pink Floyd",
+        "song": "Us and Them",
+    }
+    
+    db_client.get_an_item(region, table_name, key_info) """
+    
+    key = {
+        "artist" : "Michael Jackson",
+        "song": "Billie Jean"
+    }
+    
+    update_values = "SET album =  :new_ablum"
+    
+    expected_values = {
+        ':new_ablum' : 'NCI'
+    }
+    
+    db_client.update_an_item(table_name=table_name,key=key,update_attributes=update_values,new_attribute_values=expected_values,region=region)
 if __name__ == "__main__":
     main()
